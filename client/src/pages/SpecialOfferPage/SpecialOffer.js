8a38e4ffab27fd7db4bf71d5006b89e2f2b93745
Vitalij Prochazka <vitek.prochazka1@gmail.com>
2023-05-21 10:06:52 +0200

    SpecialOffer.js clear

--- client/src/pages/SpecialOfferPage/SpecialOffer.js
+++ client/src/pages/SpecialOfferPage/SpecialOffer.js
@@ -1,22 +1,11 @@
 import "./SpecialOffer.scss";
 
 const SpecialOffer = () => {
-  const readOnlyHandler = (e) => {
-    e.preventDefault();
-    let tst = document.getElementById("test")
-    if (tst.hasAttribute('readonly')) {
-      tst.removeAttribute('readonly')
-    } else {
-      tst.setAttribute('readonly', 'readonly');
-    }
-  }
   return (
-    <div>SpecialOffer
-      <input id="test" placeholder="TEXT SEM" type="text" />
-      <input type="submit" value="ReadOnly?" onClick={(e)=>readOnlyHandler(e)} />
-    </div>
-    
-  )
-};
+    <>
+      SpecialOffer
+    </>
+  );
+}
 
 export default SpecialOffer;
