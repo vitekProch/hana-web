63b84a9804dfade5226748e5213815c67004bc54
Vitalij Prochazka <vitek.prochazka1@gmail.com>
2023-08-20 18:16:20 +0200

    Remove useless in settings.js

--- client/src/components/sliderSettings/settings.js
+++ client/src/components/sliderSettings/settings.js
@@ -1,27 +1,3 @@
-import React, { Component } from "react";
-import Slider from "react-slick";
-
-function SampleNextArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "red" }}
-      onClick={onClick}
-    />
-  );
-}
-
-function SamplePrevArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "green", height: "40px" }}
-      onClick={onClick}
-    />
-  );
-}
 
 const SliderSettings = {
     portfolioSettings: {
