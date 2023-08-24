5ab7291fc8b596937ac8dd8313e560eff3d33732
Vitalij Prochazka <vitek.prochazka1@gmail.com>
2023-08-24 22:22:35 +0200

    Special offer added

--- client/src/pages/SpecialOfferPage/SpecialOffer.js
+++ client/src/pages/SpecialOfferPage/SpecialOffer.js
@@ -1,10 +1,19 @@
 import "./SpecialOffer.scss";
+import Special1 from "../../images//Special1.jpg"
+import Special2 from "../../images//Special2.jpg"
 
 const SpecialOffer = () => {
   return (
-    <>
-      SpecialOffer
-    </>
+    <section className="news-page page-header-margin">
+      <div className="special-offer-pictures">
+        <div className="special-offer-pictures-box">
+          <img src={Special1} alt="" />
+        </div>
+        <div className="special-offer-pictures-box">
+          <img src={Special2} alt="" />
+        </div>
+      </div>
+    </section>
   );
 }
 
